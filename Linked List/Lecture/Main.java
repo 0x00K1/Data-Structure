8d82685feeb48b48a4518b47f1155b36c1ed54dc
0x00K1 <61327783+0x00K1@users.noreply.github.com>
2024-02-11 21:23:44 +0300

    Update Main.java

--- Linked List/Lecture/Main.java
+++ Linked List/Lecture/Main.java
@@ -3,7 +3,7 @@ package Lecture;
 
 public class Main {
 	public static void main(String[] args) {
-		// LinkedList list = new LinkedList(); // Generic object list, accept any data type.
+	// LinkedList list = new LinkedList(); // Generic object list, accept any data type.
         LinkedList<String> list = new LinkedList<>(); // String object list, for providing type safety.
 		
         // Add elements
